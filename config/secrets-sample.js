fe68e99d0eec5fa12e09a1195932eda11ec6c4af
Sooraj E <sooraj.e05@gmail.com>
2014-11-25 18:46:45 +0530

    adding sample secrets.js file

---
+++ config/secrets-sample.js
@@ -0,0 +1,13 @@
+/**
+ * IMPORTANT NOTICE
+ *
+ * You should never commit this file (secrets.js) to a public repository!
+ */
+
+module.exports = {
+    s3: {
+        S3_KEY: 'Key',
+        S3_SECRET: 'secret',
+        BUCKET: 'bucket'
+    }
+};
